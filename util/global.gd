d3632c84c7eb5f3cb3bb2f15dbdd628ab27e02d4
TheBrodsky <stephensbrod@gmail.com>
2025-08-03 02:58:15 -0700

    added hard boss, added recursive checking for subpools,

--- util/global.gd
+++ util/global.gd
@@ -31,8 +31,8 @@ enum BossDifficulty {
 	HARD = 3,
 }
 
-static var global_seed: int = 1
-static var use_random_seed: bool = false
+static var global_seed: int = 0
+static var use_random_seed: bool = true
 static var SequenceRNG: RandomNumberGenerator
 static var selected_difficulty: BossDifficulty = BossDifficulty.EASY
 
