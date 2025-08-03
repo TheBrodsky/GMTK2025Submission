05bd67960210e6ba04489b6fc67c3dceeef48d84
MightyDan <daniel.habarov@avatudkool.ee>
2025-08-03 16:30:07 +0300

    Even more cool visual test

--- scenes/label_hard.gd
+++ scenes/label_hard.gd
@@ -1,5 +1,5 @@
 extends Label
 
 func _ready() -> void:
-	text = "CLONE #" + str(UserSettings.attempt_counter).pad_zeros(3) + " FAILED TO OUTPERFORM PREDECESSORS"
+	text = "CLONE #" + str(UserSettings.attempt_counter).pad_zeros(3)
 	UserSettings.attempt_counter = 0
